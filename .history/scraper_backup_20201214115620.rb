091ba1a8da63d4388d30e131e3422da1d8114df7
realisticattorney <g.axl.aquila@gmail.com>
2020-12-14 12:34:57 -0300

    parse each cryptocurrency data separately

--- .history/scraper_backup_20201214115620.rb
+++ .history/scraper_backup_20201214115620.rb
@@ -12,4 +12,18 @@ def scraper
    per_page = parsed_page.css('tbody tr').count
    total_currencies = parsed_page.css('div.sc-16r8icm-0.sc-8ccaqg-0.eEiCJF  p.sc-1eb5slv-0.kDEzev').text.split(" ")[-1]
    last_page = (total_currencies.to_f / (per_page.to_f - 1)).round
+   currencies = []
+   i = 0
+   top_10.each do |top_currency|
+      top_currency = i
+      currency = {
+         price: prices = parsed_page.css('tbody tr td div.price___3rj7O a.cmc-link')[top_currency].text,
+         name: parsed_page.css('tbody tr td div.price___3rj7O a')[top_currency].attributes["href"].value
+      }
+      i += 1
+      currencies << currency
+   end
+   byebug
+   end
    
+   scraper
