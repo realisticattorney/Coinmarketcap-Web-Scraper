1bc9c91b8c5ea5718ba07163e876d6069e7f1ba6
realisticattorney <g.axl.aquila@gmail.com>
2020-12-15 20:00:11 -0300

    remove needless history

--- .history/lib/constants_20201215194610.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-ASSET_TYPE = {
-  Crypto: 'https://coinmarketcap.com/',
-  Derivatives: 'https://coinmarketcap.com/derivatives/',
-  DeFi: 'https://coinmarketcap.com/defi/'
-}.freeze
