4c703c1b6c851402ae2a2263e572675e4a6a7309
realisticattorney <g.axl.aquila@gmail.com>
2020-12-16 13:41:09 -0300

    remove history folder

--- .history/lib/constants_20201216125117.rb
+++
@@ -1,4 +0,0 @@
-ASSET_TYPE = {
-  Crypto: 'https://coinmarketcap.com/',
-  DeFi: 'https://coinmarketcap.com/defi/'
-}.freeze
--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 ASSET_TYPE = {
   Crypto: 'https://coinmarketcap.com/',
   DeFi: 'https://coinmarketcap.com/defi/'
